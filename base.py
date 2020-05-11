55392edca559a95da053e19543f886a39757767a
casscabrera <cass.cabrera99@gmail.com>
2020-05-11 09:24:42 -0700

    populate world data

--- base.py
+++ base.py
@@ -1,16 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests, json
 from flask_bootstrap import Bootstrap 
+from api import getStat, getKeys, getWorld
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+#the route for the home page
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('home.html',
+		options = getKeys(),
+		world = getWorld()
+		)
 
+#the route for the what to do page
 @app.route('/whattodo')
 def whattodo():
 	return render_template('whattodo.html')
 
+#the route for the mythbusters page
 @app.route('/mythbusters')
 def mythbuster():
 	return render_template('mythbuster.html')
+
+#the route for redirecting the search
+@app.route('/search', methods=['GET']) 
+def search():
+	country = request.args.get('region')
+	data = getStat(country)
+	return render_template('home.html',
+		data = data["data"]["summary"],
+		options = getKeys(), 
+		country = country
+		)
